b814155dffdff5d31aed2635c22a2f627e33509b
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2024-12-27 23:54:30 +0530

    made changes in the prisma

--- src/playground.ts
+++ src/playground.ts
@@ -1,7 +1,11 @@
-// import { db } from "./server/db";
+    import { db } from "./server/db";
 
-// await db.user.create({
-//     data:[
-
-//     ]
-// })
+    await db.user.create({
+        data: {
+          emailAddress: "testemail@gmail.com",
+          firstName: "Nitin",
+          imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_k9IjkXAaiUD5vn_MAfEG7odycuC0wwOXbw&s",
+        },
+      });
+      
+      console.log("done");
